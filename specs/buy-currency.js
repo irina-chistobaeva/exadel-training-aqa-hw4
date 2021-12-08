b63b0d0a1d99825ae5f1030dcefaf163255cbeea
Iryna Chystabayeva <chistobaevairina@gmail.com>
2021-12-08 17:02:41 +0300

    Refactoring hw #4 task 3

--- specs/buy-currency.js
+++ specs/buy-currency.js
@@ -7,13 +7,14 @@ describe('Check app', function () {
         await $('#spinner').waitForDisplayed({ reverse: true });
     })
 
-    function waitForWriting(selector, index, timeout, amount) {
+    let amountArr = [];
+    function waitForWriting(selector, timeout, amount) {
+        amountArr.push({ num: amount });
         return browser.waitUntil(
             async () => {
-                const recordetAmountObj = await $(selector).getHTML(false);
-                const amountObj = { num: amount };
+                const recordedAmountArr = await $(selector).getHTML(false);
                 try {
-                    await expect(JSON.parse(recordetAmountObj)[index]).toEqual(amountObj);
+                    await expect(JSON.parse(recordedAmountArr)).toEqual(amountArr);
                 }
                 catch {
                     return false;
@@ -28,13 +29,13 @@ describe('Check app', function () {
 
     it('Buy currency: 1234', async function () {
         await $('#sum-to-buy').setValue('1');
-        await waitForWriting('script#database', 0, 5000, '1');
+        await waitForWriting('script#database', 5000, '1');
         await $('#sum-to-buy').setValue('2');
-        await waitForWriting('script#database', 1, 5000, '2');
+        await waitForWriting('script#database', 5000, '2');
         await $('#sum-to-buy').setValue('3');
-        await waitForWriting('script#database', 2, 5000, '3');
+        await waitForWriting('script#database', 5000, '3');
         await $('#sum-to-buy').setValue('4');
-        await waitForWriting('script#database', 3, 5000, '4');
+        await waitForWriting('script#database', 5000, '4');
         await $('//button[contains(.,"Buy")]').click();
         const rate = await $('#currency-rate').getText();
         const resultStr = await $('#withdrew').getText();
